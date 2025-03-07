5dfad880f44048bbdc77132641f9474c6cc7cc28
FBEmpire <109236321+FBEmpire@users.noreply.github.com>
2025-03-07 17:30:06 +0500

    Update client.py

--- client.py
+++ client.py
@@ -42,11 +42,31 @@ print("         |         |    \  /    |   |          |   |    \     |          
 print("         |______   |     \/     |   |          |   |     \    |______           ")
 print("                                                                                   site: fbe.leyixgame.ru")
 
+# Запрос режима работы
 mode = input("mode (1 - upload; 2 - download): ")
 
 if mode == '1':
+    # Запрос логина, пароля и секретного кода только для загрузки
+    username = input("Введите логин: ")
+    password = input("Введите пароль: ")
+    secret_key = input("Введите секретный код: ")
+    # Проверка авторизации
+    auth_response = requests.post('http://sql.leyixgame.ru:5000/auth', json={
+        'username': username,
+        'password': password,
+        'secret_key': secret_key
+    })
+  
+    if auth_response.status_code != 200:
+        print("Ошибка авторизации:", auth_response.json().get('error', 'Неизвестная ошибка'))
+        exit()
+
+    print("Авторизация успешна!")
+
+    # Запрос имени файла для загрузки
     name = input("filename: ")
 elif mode == '2':
+    # Запрос имени таблицы для скачивания
     table = input("tablename: ")
 else:
     print("Invalid mode selected.")
@@ -56,10 +76,15 @@ def upload_image(file_path):
     # Извлечение формата файла
     file_format = file_path.split('.')[-1]  # Получаем расширение файла
     with open(file_path, 'rb') as img_file:
-        base64_data = base64.b64encode(img_file.read()).decode('utf-8')
+        base64_data = base64.b64encode(img_file.read()).decode('utf-8')  # Преобразуем файл в base64
     
     # Отправка данных на сервер
     response = requests.post('http://sql.leyixgame.ru:5000/upload', json={
+        'auth': {
+            'username': username,
+            'password': password,
+            'secret_key': secret_key
+        },
         'image': base64_data,
         'file_format': file_format
     })
@@ -70,7 +95,10 @@ def upload_image(file_path):
         print("Response content is not valid JSON:", response.text)
 
 def download_images(table_name):
-    response = requests.post('http://sql.leyixgame.ru:5000/download', json={'table_name': table_name})
+    # Отправка данных на сервер для скачивания
+    response = requests.post('http://sql.leyixgame.ru:5000/download', json={
+        'table_name': table_name
+    })
 
     if response.status_code == 200:
         # Сохраняем файл на клиенте
